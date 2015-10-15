9bcfef7d03200e39456ee0f448344e45020e1988
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2015-10-15 14:53:47 +0200

    Change the spot creation strategy for CellContactDetector.
    Now spots are directly created from the mask of contacts. Their position
    are the mean position of the pixel in each binary object, and we deduce
    its radius from the object size. Quality value stores the total
    intensity of the contact image within the object. The threshold is used
    to discard contacts with a small size.
    
    Signed-off-by: Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>

--- src/main/java/fr/pasteur/trackmate/CellContactDetector.java
+++ src/main/java/fr/pasteur/trackmate/CellContactDetector.java
@@ -1,20 +1,31 @@
 package fr.pasteur.trackmate;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 
+import net.imglib2.Cursor;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.algorithm.Benchmark;
 import net.imglib2.algorithm.MultiThreaded;
+import net.imglib2.algorithm.labeling.AllConnectedComponents;
 import net.imglib2.img.Img;
 import net.imglib2.img.ImgFactory;
+import net.imglib2.img.planar.PlanarImgFactory;
+import net.imglib2.labeling.NativeImgLabeling;
+import net.imglib2.roi.IterableRegionOfInterest;
 import net.imglib2.type.NativeType;
+import net.imglib2.type.logic.BitType;
 import net.imglib2.type.numeric.RealType;
+import net.imglib2.type.numeric.integer.UnsignedIntType;
 import net.imglib2.util.Util;
 import fiji.plugin.trackmate.Spot;
-import fiji.plugin.trackmate.detection.LogDetector;
 import fiji.plugin.trackmate.detection.SpotDetector;
 import fr.pasteur.ContactImgGenerator;
+import fr.pasteur.util.Thresholder;
 
+@SuppressWarnings( "deprecation" )
 public class CellContactDetector< T extends RealType< T > & NativeType< T >> implements SpotDetector< T >, MultiThreaded, Benchmark
 {
 
@@ -104,16 +115,86 @@ public class CellContactDetector< T extends RealType< T > & NativeType< T >> imp
 			return false;
 		}
 		
-		final double radius = 2.0 * contactSize;
-		final LogDetector< T > detector = new LogDetector< T >( out, out, calibration, radius, threshold, false, false );
-		detector.setNumThreads( numThreads );
-		if ( !detector.checkInput() || !detector.process() )
-		{
-			errorMessage = BASE_ERROR_MSG + detector.getErrorMessage();
-			return false;
-		}
+		
+		/*
+		 * Generate binary mask.
+		 */
 
-		spots = detector.getResult();
+		final ImgFactory< BitType > maskFactory = Util.getArrayOrCellImgFactory( im1, new BitType() );
+		final Img< BitType > mask = maskFactory.create( im1, new BitType() );
+		final T valTreshold = out.firstElement().createVariable();
+		valTreshold.setZero();
+		Thresholder.threshold( out, mask, valTreshold, true, numThreads );
+
+		/*
+		 * Find connected components.
+		 */
+
+		final Iterator< Integer > names = AllConnectedComponents.getIntegerNames( 0 );
+		final Img< UnsignedIntType > lbl = new PlanarImgFactory< UnsignedIntType >().create( mask, new UnsignedIntType() );
+		final NativeImgLabeling< Integer, UnsignedIntType > labeling = new NativeImgLabeling< Integer, UnsignedIntType >( lbl );
+		AllConnectedComponents.labelAllConnectedComponents( labeling, mask, names );
+
+		/*
+		 * Loop over connected components and generate spots.
+		 */
+
+		final Collection< Integer > labels = labeling.getLabels();
+		spots = new ArrayList< Spot >( labels.size() );
+		for ( final Integer label : labels )
+		{
+			final long size = labeling.getArea( label );
+			if ( size < threshold )
+			{
+				continue;
+			}
+
+
+			final IterableRegionOfInterest roi = labeling.getIterableRegionOfInterest( label );
+			final Cursor< T > cursor = roi.getIterableIntervalOverROI( out ).cursor();
+			final double[] lpos = new double[ im1.numDimensions() ];
+			final double[] pos = new double[] { 0., 0., 0. };
+			double x = 0.;
+			double y = 0.;
+			double z = 0.;
+			double quality = 0.;
+			while ( cursor.hasNext() )
+			{
+				cursor.fwd();
+				cursor.localize( lpos );
+				System.arraycopy( lpos, 0, pos, 0, lpos.length );
+				x += pos[ 0 ] * calibration[ 0 ];
+				y += pos[ 1 ] * calibration[ 1 ];
+				z += pos[ 2 ] * calibration[ 2 ];
+				quality += cursor.get().getRealDouble();
+			}
+			// Spot position is the mean position of all pixels from the region.
+			x /= size;
+			y /= size;
+			z /= size;
+
+			double volume = size;
+			for ( int d = 0; d < im1.numDimensions(); d++ )
+			{
+				volume *= calibration[ d ];
+			}
+			final double radius;
+			switch ( im1.numDimensions() )
+			{
+			case 1:
+				radius = volume / 2;
+				break;
+			case 2:
+			default:
+				radius = Math.sqrt( volume / Math.PI );
+				break;
+			case 3:
+				radius = Math.pow( 3. * volume / 4. / Math.PI, 1. / 3. );
+			}
+
+			final Spot spot = new Spot( x, y, z, radius, quality );
+			spots.add( spot );
+		}
 
 		final long end = System.currentTimeMillis();
 		this.processingTime = end - start;
